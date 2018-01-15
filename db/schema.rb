4c32630207f813f092e75001553a6b8b325798ed
ScottB <17sb@cis.dk>
2018-01-15 20:09:01 +0100

    Add stripe keys

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 20180115154343) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "restaurant_id"
     t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
