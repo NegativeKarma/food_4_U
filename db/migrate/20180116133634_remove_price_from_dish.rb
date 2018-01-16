7808aa311e41ab2e4173c2a5a42588d5f2eacec5
ScottB <17sb@cis.dk>
2018-01-16 16:21:12 +0100

    Remove redundant migration file and merge schema file

--- db/migrate/20180116133634_remove_price_from_dish.rb
+++
@@ -1,5 +0,0 @@
-class RemovePriceFromDish < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :dishes, :price, :integer
-  end
-end
--- db/migrate/20180116133813_add_price_to_dish.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToDish < ActiveRecord::Migration[5.1]
-  def change
-    add_column :dishes, :price, :float
-  end
-end
